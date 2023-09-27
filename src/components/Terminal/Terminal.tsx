6d254414da5a3ee2ec4cd4ddc82b69a2e319eb35
Nehuen Cisneros <cisneros.n.b@outlook.com>
2023-09-26 22:21:44 -0300

    ARREGLOS EN LOS SELECTS

--- src/components/Terminal/Terminal.tsx
+++ src/components/Terminal/Terminal.tsx
@@ -24,16 +24,18 @@ export const Terminal: React.FC<{}> = () => {
                      <h5 className={style.compiled}>Compiled successfully!</h5>
                      <h5 className={style.compiled}>No issues found.</h5>
                      <h5 className={style.corte}>^C</h5>
-                     <h4><h5 className={style.commandLine}>Nehuen@Desktop {'> '}</h5>Soy un meticuloso y responsable desarrollador IT, que a fines de 2021 comenzó su camino estudiando informática.
+                     <h4><h4 className={style.commandLine}>Nehuen@Desktop {'> '}</h4>Soy un meticuloso y responsable desarrollador IT, que a fines de 2021 comenzó su camino estudiando informática.
                         Enfrentarme a un problema y tener la capacidad para resolverlo, es el porqué de haberme volcado a esta rama de la tecnología, y
                         la posibilidad de crear una app o una web, son el motor de que haya elegido la carrera de FullStack Developer en Henry.
                         Cuento con la paciencia y la dedicación adecuada para encarar y superar los desafíos que este rubro propone, y gracias a
                         haberme desempeñado más de 10 años en tareas relacionadas con la atención al cliente, colaboradores y proveedores,
                         relacionarme con otros e integrarme en grupos no me representa una dificultad. Desde ya, agradezco el tiempo tomado para leer esta minibio.
                      </h4>
-                     <h5 className={style.commandLine}>Seleccione</h5>
+                     <h4 className={style.commandLine}>Para empezar escriba o seleccione :</h4>
                      <div className={style.selects}>
                         <SelectButton name={"proyectos"} number={"1"}/>
+                        <SelectButton name={"skills"} number={"2"}/>
+                        <SelectButton name={"contacto"} number={"3"}/>
                      </div>
                   </div>
                </div>
--- src/styled/Selects/Select.tsx
+++ src/styled/Selects/Select.tsx
@@ -6,15 +6,18 @@ type ButtonSelectProps = {
    number: string
 }
 
-export const SelectButton: React.FC<ButtonSelectProps> = ({name, number}) => {
+export const SelectButton: React.FC<ButtonSelectProps> = ({ name, number }) => {
 
-   return(
-      <div style={{display: "flex", alignItems: "center"}}>
-         <h4>
-            {number}
+   return (
+      <div style={{ display: "flex", alignItems: "center" }}>
+         <h4 className={style.textH4}>
+            [{number}] 
          </h4>
+         <h5 className={style.textH5}>o</h5>
          <button className={style.button}>
-            {name}
+               ir a {name}
+            <div className={style.container}>
+            </div>
          </button>
       </div>
    )
